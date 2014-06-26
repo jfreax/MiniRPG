33af15198df1821c98a7e50359644d6fea0abfdc
Jens Dieskau <jens@dieskau.pm>
2014-06-27 00:19:17 +0200

    Update and fps counter

--- web/game_world.dart
+++ web/game_world.dart
@@ -8,8 +8,12 @@ class GameWorld extends PolymerElement {
   @published num height = 0;
   @published bool debug = false;
   
+  // private
+  num _renderTime;
+  num _fpsAverage;
+  SpanElement _fpsDisplay;
+
   DivElement _renderer;
-  SpanElement _fpsCounter;
 
 
   GameWorld.created() : super.created() {
@@ -19,17 +23,36 @@ class GameWorld extends PolymerElement {
     }
     
     _renderer = $['renderer'] as DivElement;
-    _fpsCounter = $['debugFPS'] as SpanElement;
-    _fpsCounter.text = "0";
+    _fpsDisplay = $['debugFPS'] as SpanElement;
+    _fpsDisplay.text = "0";
     
     
     addNode(100, 0);
     addNode(50, 80);
+    
+    update(0);
   }
   
-  addNode(num x, num y) {
+  void addNode(num x, num y) {
     _renderer.children.add(new Element.tag('game-node')..attributes["x"] = x.toString()..attributes["y"] = y.toString());
   }
 
+  void update(num _) {
+    num time = new DateTime.now().millisecondsSinceEpoch;
+    if (_renderTime != null) showFps(1000 / (time - _renderTime));
+    _renderTime = time;
+    
+    requestUpdate();
+  }
+  
+  void requestUpdate() {
+    window.requestAnimationFrame(update);
+  }
+  
+  void showFps(num fps) {
+    if (_fpsAverage == null) _fpsAverage = fps;
+    _fpsAverage = fps * 0.05 + _fpsAverage * 0.95;
+    _fpsDisplay.text = "${_fpsAverage.round()} fps";
+  }
 
 }
