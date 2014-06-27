cfd650287abb30bc1550216ca6ad5d3a0b3c0f22
Jens Dieskau <jens@dieskau.pm>
2014-06-27 12:46:18 +0200

    Workaround for node positioning with ShadowDOM polyfill

--- web/game_node.dart
+++ web/game_node.dart
@@ -18,5 +18,14 @@ class GameNode extends PolymerElement {
   {
     attributes["x"] = x.toString();
     attributes["y"] = y.toString();
+    
+    // WORKAROUND
+    // ShadowDOM Polyfill does not support bindings in style elements
+    // https://github.com/Polymer/polymer/issues/456
+    style.left = "${x}px";
+    style.top = "${y}px";
+    this.shadowRoot.querySelector('.fab').style
+      ..width = "${radius}px"
+      ..height = "${radius}px";
   }
 }
