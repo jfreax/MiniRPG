41eb027b28028ece53983d012d50a7fb878dcc70
Jens Dieskau <jens@dieskau.pm>
2014-06-27 10:06:32 +0200

    Correct node positioning

--- web/game_node.dart
+++ web/game_node.dart
@@ -1,12 +1,20 @@
+library mini_rpg.gamenode;
+
 import 'package:polymer/polymer.dart';
 //import 'dart:html';
 
 @CustomTag('game-node')
-class Node extends PolymerElement {
+class GameNode extends PolymerElement {
   // attributes
   @published num x = 0;
   @published num y = 0;
   @published String type = "empty";
   
-  Node.created() : super.created();
+  GameNode.created() : super.created();
+  
+  void setPosition(num x, num y)
+  {
+    attributes["x"] = x.toString();
+    attributes["y"] = y.toString();
+  }
 }
--- web/game_world.dart
+++ web/game_world.dart
@@ -1,9 +1,19 @@
 import 'package:polymer/polymer.dart';
 import 'dart:html';
+import 'dart:math';
+
+import 'game_node.dart';
 
 @CustomTag('game-world')
 class GameWorld extends PolymerElement {
+  final int MIN_WIDTH = 800;
+  final int MIN_HEIGHT= 600;
+  
+  final Random rng = new Random();
+  
   // attributes
+  @published num size = 5;
+  @published num numberOfPlayer = 2;
   @published bool debug = false;
   
   // private
@@ -14,23 +24,34 @@ class GameWorld extends PolymerElement {
   DivElement _renderer;
 
 
-  GameWorld.created() : super.created() {
+  GameWorld.created() : super.created()
+  {
     _renderer = $['renderer'] as DivElement;
     _fpsDisplay = $['debugFPS'] as SpanElement;
     _fpsDisplay.text = "0";
     
-    
-    addNode(100, 0);
-    addNode(50, 80);
-    
+    generateMap();
     requestUpdate();
   }
   
-  void addNode(num x, num y) {
-    _renderer.children.add(new Element.tag('game-node')..attributes["x"] = x.toString()..attributes["y"] = y.toString());
+  void generateMap()
+  {
+    _addNode(100, 0);
+    _addNode(100, 80);
+    
+  }
+  
+  void _addNode(num x, num y)
+  {
+    GameNode newNode = new Element.tag('game-node') as GameNode;
+    newNode.setPosition(x, y);
+
+    
+    _renderer.children.add(newNode);
   }
 
-  void update(num _) {
+  void _update(num _)
+  {
     num time = new DateTime.now().millisecondsSinceEpoch;
     if (_renderTime != null) showFps(1000 / (time - _renderTime));
     _renderTime = time;
@@ -38,14 +59,22 @@ class GameWorld extends PolymerElement {
     requestUpdate();
   }
   
-  void requestUpdate() {
-    window.requestAnimationFrame(update);
+  void requestUpdate()
+  {
+    window.requestAnimationFrame(_update);
   }
   
   void showFps(num fps) {
     if (_fpsAverage == null) _fpsAverage = fps;
     _fpsAverage = fps * 0.05 + _fpsAverage * 0.95;
     _fpsDisplay.text = "${_fpsAverage.round()} fps";
+  }
+  
+  // input events
+  keyup(KeyboardEvent e, var detail, Node target)
+  {
+    print("OK");
+    print("Key $e.keyCode");
   }
 
 }
